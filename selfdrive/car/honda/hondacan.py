f98df859b633db7ce1791815cf37c5e91bf7a23b
DESKTOP-NVQVLIB\david <davidzachariah@yahoo.com>
2019-09-17 20:13:29 -0700

    Updates
    Updates

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -63,24 +63,17 @@ def create_brake_command(packer, apply_brake, pcm_override, pcm_cancel_cmd, fcw,
     "CHIME": 0,
     # TODO: Why are there two bits for fcw? According to dbc file the first bit should also work
     "FCW": fcw << 1,
+    #NOT USED! Clarity
     "AEB_REQ_1": 0,
     "AEB_REQ_2": 0,
     "AEB_STATUS": 0,
+    #NOT USED! Clarity
   }
   #Clarity
   #bus = get_pt_bus(car_fingerprint, is_panda_black)
   #return packer.make_can_msg("BRAKE_COMMAND", bus, values, idx)
   commands.append(packer.make_can_msg("BRAKE_COMMAND", bus, values, idx))
   return commands
-
-#Clarity
-def create_gas_command(packer, gas_amount, idx):
-  enable = gas_amount > 0.001
-  values = {"ENABLE": enable}
-  if enable:
-    values["GAS_COMMAND"] = gas_amount * 255.
-    values["GAS_COMMAND2"] = gas_amount * 255.
-  return packer.make_can_msg("GAS_COMMAND", 0, values, idx)
 
 def create_steering_control(packer, apply_steer, lkas_active, car_fingerprint, idx, is_panda_black):
   values = {
