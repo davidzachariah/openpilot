7df1bd4efe738075c86098cb4f4512107f3cdb40
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2019-12-24 20:42:01 -0500

    Add Clarity Support to 0.7

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -494,7 +494,7 @@ def controlsd_thread(sm=None, pm=None, can_sock=None):
 
   car_recognized = CP.carName != 'mock'
   # If stock camera is disconnected, we loaded car controls and it's not chffrplus
-  controller_available = CI.CC is not None and not passive #Clarity
+  controller_available = CI.CC is not None and not passive
   community_feature_disallowed = CP.communityFeature and not community_feature_toggle
   read_only = not car_recognized or not controller_available or CP.dashcamOnly or community_feature_disallowed
   if read_only:
